484ff40c02b8cd80703119adf6231075dd114c5b
Mateus Lopes <73197343+progmateus@users.noreply.github.com>
2022-11-01 22:06:26 -0300

    last commit

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 IMAGE_URL="https://supermariorun.com/assets/img/hero/hero_chara_mario_update_pc.png"
 BACKGROUND_URL="https://i.pinimg.com/originals/8f/1a/b8/8f1ab8b14a55720d6859620a6f596933.jpg"
-LOGO_URL=https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Mario_Series_Logo.svg/1200px-Mario_Series_Logo.svg.png
+LOGO_URL="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Mario_Series_Logo.svg/1200px-Mario_Series_Logo.svg.png"
 
 
 curl "http://localhost:3000/joinImages?img=$IMAGE_URL&background=$BACKGROUND_URL&logo=$LOGO_URL"
